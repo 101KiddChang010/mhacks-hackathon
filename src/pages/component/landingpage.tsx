7e29fccf3bd55cfdbeb0d55abc4d5900e31ba5da
Kidd <kiddochang02@gmail.com>
2023-02-18 05:32:44 -0500

    Implement landing page further

--- src/pages/component/landingpage.tsx
+++ src/pages/component/landingpage.tsx
@@ -13,14 +13,69 @@ export default function LandingPage() {
           management with Claire.
         </p>
         <div className="flex gap-2">
-          <button className="h-8 w-[6.25rem] rounded-2xl bg-secondary text-center text-sm text-white">
-            Get Started
-          </button>
-          <button className="h-8 w-[6.25rem] rounded-2xl bg-primary text-center text-sm text-white">
-            GitHub Repo
-          </button>
+          <a
+            href=""
+            className="flex h-8 w-[6.25rem] items-center justify-center rounded-2xl bg-secondary text-center text-sm text-white"
+          >
+            <span>Get Started</span>
+          </a>
+          <a
+            href="https://github.com/101KiddChang010/mhacks-hackathon"
+            className="flex h-8 w-[6.25rem] items-center justify-center rounded-2xl bg-primary text-center text-sm text-white"
+          >
+            <span>GitHub Repo</span>
+          </a>
         </div>
         <img src="showcase.png" alt="" className="my-8" />
+        <div className="mt-[4.688rem] mb-[3.125rem]">
+          <h2 className="text-2xl font-bold tracking-[.12rem] text-secondary">
+            WHAT IS CLAIRE
+          </h2>
+          <p className="mt-[.5rem] text-sm font-medium">
+            The innovative and intelligent solution to streamline your
+            healthcare needs. Claire is an AI designed to assist you with
+            personalized medical advice and connect you with the best healthcare
+            providers. With the latest cutting-edge technology and advanced
+            machine learning algorithms, our AI is capable of analyzing medical
+            data, identifying symptoms, and providing accurate diagnoses. From
+            to monitoring your health to scheduling appointments, Claire is your
+            one-stop shop for all your medical needs. It’s a convenient and
+            efficient way of managing your healthcare.
+          </p>
+        </div>
+        <div className="">
+          <h2 className="text-2xl font-bold tracking-[.12rem] text-secondary">
+            HOW CLAIRE WORKS
+          </h2>
+          <p className="mt-[.5rem] text-sm font-medium">
+            Claire is an innovative medical assistant AI that uses the latest
+            breakthroughs in natural language processing and artificial
+            intelligence to help people take charge of their health. Developed
+            with the state-of-the-art GPT-3 language model, Claire is able to
+            understand and respond to natural language queries in a way that is
+            both accurate and empathetic. Whether you have a specific health
+            concern or just need general guidance, Claire is here to help you
+            every step of the way. <br />
+            <br /> Claire's powerful capabilities are made possible by GPT-3's
+            advanced machine learning algorithms, which allow it to analyze vast
+            amounts of medical data and generate personalized responses based on
+            your individual needs. With Claire, you can get quick and accurate
+            medical advice, schedule appointments, and connect with healthcare
+            providers from the comfort of your own home. Whether you're looking
+            to manage a chronic condition or just want to stay healthy, Claire
+            is your partner in achieving optimal health and well-being. <br />
+            <br />
+            One of the key benefits of Claire is its ability to adapt and learn
+            as it interacts with patients. As it receives more data and
+            feedback, Claire's responses become more accurate and personalized.
+            This means that over time, Claire can become an even more effective
+            medical assistant, providing patients with the most up-to-date and
+            relevant information. With its cutting-edge technology and
+            commitment to patient care, Claire is revolutionizing the healthcare
+            industry and transforming the way people think about medical
+            assistance.
+          </p>
+        </div>
       </div>
     </>
   );
