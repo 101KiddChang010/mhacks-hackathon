feca5851a38266f7c18b59493c5ff2d02e00d617
Kidd <kiddochang02@gmail.com>
2023-02-18 05:36:48 -0500

    Space nav closer to end

--- src/pages/component/navbar.tsx
+++ src/pages/component/navbar.tsx
@@ -4,11 +4,11 @@ export default function Navbar() {
   return (
     <>
       <header className="my-2">
-        <nav className="flex w-full items-center justify-evenly">
-          <p className="text-2xl font-bold">
+        <nav className="flex w-full items-center justify-between">
+          <p className="mx-10 text-2xl font-bold">
             Cura<span className="text-primary">AI</span>
           </p>
-          <button className="h-8 w-20 rounded-2xl bg-secondary text-center text-white">
+          <button className="mx-10 h-8 w-20 rounded-2xl bg-secondary text-center text-white">
             Log In
           </button>
         </nav>
