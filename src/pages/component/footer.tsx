c65a1cf8642b0ddb975ae5c787ff0fd4fb34b79b
Kidd <kiddochang02@gmail.com>
2023-02-18 12:41:32 -0500

    Change margin --> padding

--- src/pages/component/footer.tsx
+++ src/pages/component/footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer(props: any) {
   return (
     <>
-      <div>
+      <div className="py-2">
         <p
-          className={`my-2 flex items-center justify-center text-sm font-medium tracking-[.165rem] ${props.color}`}
+          className={`flex items-center justify-center text-sm font-medium tracking-[.165rem] ${props.color}`}
         >
           Copyright &copy; 2023 | Bryan Lor & Kidd Chang
         </p>
