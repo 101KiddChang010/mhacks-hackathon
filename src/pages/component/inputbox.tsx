60da4859f68155f3bde16ce7d1b93042f5e77ad7
Kidd <kiddochang02@gmail.com>
2023-02-18 15:59:51 -0500

    Add box style

--- src/pages/component/inputbox.tsx
+++ src/pages/component/inputbox.tsx
@@ -2,10 +2,10 @@ export default function InputBox() {
   return (
     <>
       <div className="flex items-center justify-center">
-        <form action="" className="flex">
+        <form action="" className="flex shadow-xl">
           <input
             type="text"
-            className="shadow-[0_4px_4px_0_rgba(0, 0, 0, 0.25)] h-[2.75rem] w-[50rem] rounded-l-2xl bg-[#6a6a6a] pl-5 text-white focus:outline-none"
+            className="h-[2.75rem] w-[50rem] rounded-l-2xl bg-[#6a6a6a] pl-5 text-white focus:outline-none"
           />
           <div className="flex h-[2.75rem] w-[2.5rem] items-center justify-start rounded-r-2xl bg-[#6a6a6a]">
             <button className="pl-1">
