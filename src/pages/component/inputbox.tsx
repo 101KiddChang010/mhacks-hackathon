4a589ac3ccf6117459cc2c69b8d1ef6ab6eab6f6
Kidd <kiddochang02@gmail.com>
2023-02-18 15:47:19 -0500

    Style user input

--- src/pages/component/inputbox.tsx
+++ src/pages/component/inputbox.tsx
@@ -2,12 +2,16 @@ export default function InputBox() {
   return (
     <>
       <div className="flex items-center justify-center">
-        <form action="">
+        <form action="" className="flex">
           <input
             type="text"
-            className="shadow-[0_4px_4px_0_rgba(0, 0, 0, 0.25)] h-[2.75rem] w-[50rem] rounded-2xl bg-[#7D7D7D]"
+            className="shadow-[0_4px_4px_0_rgba(0, 0, 0, 0.25)] h-[2.75rem] w-[50rem] rounded-l-2xl bg-[#6a6a6a] pl-5 text-white focus:outline-none"
           />
-          <i className="fa-solid fa-paper-plane"></i>
+          <div className="flex h-[2.75rem] w-[2.5rem] items-center justify-start rounded-r-2xl bg-[#6a6a6a]">
+            <button className="pl-1">
+              <i className="fa-regular fa-paper-plane fa-xl text-white"></i>
+            </button>
+          </div>
         </form>
       </div>
     </>
