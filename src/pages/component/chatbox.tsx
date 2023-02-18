179bae673cad881679dc89b18ba8cdf32af78c06
Kidd <kiddochang02@gmail.com>
2023-02-18 18:50:06 -0500

    Implement ui chat

--- src/pages/component/chatbox.tsx
+++ src/pages/component/chatbox.tsx
@@ -1,7 +1,45 @@
-export default function ChatBox() {
+export default function ChatBox(props: any) {
   return (
     <>
-      <div className="h-[16.5rem]"></div>
+      <div className="h-[16.5rem]">
+        <UserChat />
+        <AiChat />
+      </div>
     </>
   );
 }
+
+const UserChat = (props: any) => {
+  return (
+    <>
+      <div className="flex h-20 items-center justify-center bg-chatbox-light">
+        <div className="flex w-[90vw] text-left">
+          <p className="text-whitepr-5 w-[3.125rem]"></p>
+          <p className="pl-5 text-white">
+            Hello Claire, I am feeling a bit tired lately and feel weak in
+            general. What do you recommend me to do?
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const AiChat = (props: any) => {
+  return (
+    <>
+      <div className="flex h-20 items-center justify-center">
+        <div className="flex w-[90vw] text-left">
+          <p className="w-[3.125rem] text-white ">Claire:</p>
+          <p className="pl-5 text-white">
+            I recommend that you get plenty of rest, stay hydrated, and eat a
+            balanced diet. Additionally, you may want to consider taking a
+            multivitamin or supplement to ensure you are getting all the
+            necessary nutrients. Exercise can also help boost your energy
+            levels.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
