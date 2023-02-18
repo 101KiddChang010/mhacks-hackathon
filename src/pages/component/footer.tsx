b7f8b5fbfba971215a3e28389bc282a9ba382cb4
Kidd <kiddochang02@gmail.com>
2023-02-18 05:58:43 -0500

    Better styled footer

--- src/pages/component/footer.tsx
+++ src/pages/component/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <>
       <div>
-        <p className="flex items-center justify-center text-sm">
+        <p className="my-2 flex items-center justify-center text-sm font-medium tracking-[.165rem] text-[#323232]">
           Copyright &copy; 2023 | Bryan Lor & Kidd Chang
         </p>
       </div>
