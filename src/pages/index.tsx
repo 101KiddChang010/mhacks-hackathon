9c245ac91eb21810073c375bbe7cad4a92d5e18f
Kidd <kiddochang02@gmail.com>
2023-02-19 04:53:35 -0500

    Create responsive design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,11 +16,11 @@ const Home: NextPage = () => {
       <Navbar />
       <div className="my-0 mx-auto flex max-w-screen-lg flex-1 flex-col items-center justify-center">
         <div className="mt-[4rem] flex flex-col items-center justify-center">
-          <h1 className="text-center text-5xl font-semibold">
+          <h1 className="text-center text-3xl font-semibold sm:text-5xl">
             Introducing Cl
             <span className="text-primary">ai</span>re
           </h1>
-          <p className="my-4 w-[56.5rem] text-center text-2xl">
+          <p className="my-4 w-[90vw] text-center text-xs sm:text-2xl">
             Your personalized AI medical assistant. Get accurate medical advice,
             quick diagnoses, and access to healthcare providers all in one
             place. Say goodbye to long wait times and hello to convenient
@@ -44,19 +44,19 @@ const Home: NextPage = () => {
             <img
               src="claire-talk.png"
               alt="Claire AI Image"
-              className="absolute top-0 right-[50%] z-0 h-[11.375rem] translate-x-[50%] drop-shadow-[0rem_0.125rem_0.125rem_rgba(0,0,0,0.12)] duration-200 hover:translate-y-[-0.5rem]"
+              className="sm-top-0 absolute right-[50%]  z-0 h-[10rem] translate-x-[50%] drop-shadow-[0rem_0.125rem_0.125rem_rgba(0,0,0,0.12)] duration-200 hover:translate-y-[-0.5rem] max-sm:top-5 sm:h-[11.375rem]"
             />
             <img
               src="showcase.jpg"
               alt="Image showcasing interaction with Claire"
-              className="relative z-[1] mt-[10rem] w-[48.75rem] shadow-2xl"
+              className="relative z-[1] mt-[10rem] h-auto w-[78vw] shadow-2xl"
             />
           </div>
           <div className="mt-[4.688rem]">
-            <h2 className="text-2xl font-bold tracking-[.12rem] text-secondary">
+            <h2 className="text-xl font-bold tracking-[.12rem] text-secondary sm:text-2xl">
               WHAT IS CLAIRE
             </h2>
-            <p className="mt-[.5rem] text-sm font-medium">
+            <p className="mt-[.5rem] w-[90vw] text-sm font-medium max-sm:text-xs">
               The innovative and intelligent solution to streamline your
               healthcare needs. Claire is an AI designed to assist you with
               personalized medical advice and connect you with the best
@@ -70,10 +70,10 @@ const Home: NextPage = () => {
             </p>
           </div>
           <div className="mt-[4.688rem]">
-            <h2 className="text-2xl font-bold tracking-[.12rem] text-secondary">
+            <h2 className="text-xl font-bold tracking-[.12rem] text-secondary sm:text-2xl">
               HOW CLAIRE WORKS
             </h2>
-            <p className="mt-[.5rem] text-sm font-medium">
+            <p className="mt-[.5rem] w-[90vw] text-sm font-medium max-sm:text-xs">
               Claire is an innovative medical assistant AI that uses the latest
               breakthroughs in natural language processing and artificial
               intelligence to help people take charge of their health. Developed
@@ -104,14 +104,14 @@ const Home: NextPage = () => {
             </p>
           </div>
           <div className="mt-[4.688rem] mb-[3rem]">
-            <h3 className="flex justify-center text-2xl font-bold text-[#474750]">
+            <h3 className="flex justify-center text-xl font-bold text-[#474750] sm:text-2xl">
               Powered By:
             </h3>
-            <div className="mt-5 flex gap-[5.313rem]">
+            <div className="mt-5 flex gap-7 px-4 md:gap-[5.313rem]">
               <a href="https://openai.com">
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg"
-                  className="max-h-[4.375rem] duration-300 hover:scale-110"
+                  className="max-h-[4.375rem] w-auto duration-300 hover:scale-110 md:h-[25rem]"
                   alt=""
                 />
               </a>
@@ -119,27 +119,27 @@ const Home: NextPage = () => {
                 <img
                   src="https://next-auth.js.org/img/logo/logo-sm.png"
                   alt=""
-                  className="max-h-[4.375rem] duration-300 hover:scale-110"
+                  className="max-h-[4.375rem] w-auto duration-300 hover:scale-110 md:h-[25rem]"
                 />
               </a>
               <a href="https://nextjs.org">
                 <img
                   src="https://static-00.iconduck.com/assets.00/next-js-icon-512x512-zuauazrk.png"
-                  className="max-h-[4.375rem] duration-300 hover:scale-110"
+                  className="max-h-[4.375rem] w-auto duration-300 hover:scale-110 md:h-[25rem]"
                   alt=""
                 />
               </a>
               <a href="https://reactjs.org">
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png"
-                  className="max-h-[4.375rem] duration-300 hover:scale-110"
+                  className="max-h-[4.375rem] w-auto duration-300 hover:scale-110 md:h-[25rem]"
                   alt=""
                 />
               </a>
               <a href="https://www.mongodb.com">
                 <img
                   src="https://www.svgrepo.com/show/331488/mongodb.svg"
-                  className="max-h-[4.375rem] duration-300 hover:scale-110"
+                  className="max-h-[4.375rem] w-auto duration-300 hover:scale-110 md:h-[25rem]"
                   alt=""
                 />
               </a>
