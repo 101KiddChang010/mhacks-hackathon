ddb9606a077ac898b89fd871d581473caf8e6ac9
Bryan-Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-02-18 19:17:55 -0500

    fixed main app front end

--- src/pages/component/navbar.tsx
+++ src/pages/component/navbar.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-      <header className="py-2">
-        <nav className="flex w-full items-center justify-between">
-          <p className="mx-10 text-2xl font-bold">
+      <header>
+        <nav className="mx-auto flex w-full items-center py-2 sm:w-[90vw] lg:w-[60vw]">
+          <Link href="../" className="flex-1 text-2xl font-bold">
             Cura<span className="text-primary">AI</span>
-          </p>
+          </Link>
           <button className="mx-10 h-8 w-20 rounded-2xl bg-secondary text-center text-white">
             Log In
           </button>
